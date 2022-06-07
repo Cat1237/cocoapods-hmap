56b60709671db824b5c4a7ff2a73fc960980e87a
Cat1237 <cat1237@logic.com>
2022-06-07 14:22:48 +0800

    merge project headers

--- lib/hmap/gem_version.rb
+++ lib/hmap/gem_version.rb
@@ -1,5 +1,5 @@
 module HMap
   # The version of the hmapfile command line tool.
   #
-  VERSION = '0.2.6.7'.freeze unless defined? HMap::VERSION
+  VERSION = '0.2.6.8'.freeze unless defined? HMap::VERSION
 end
