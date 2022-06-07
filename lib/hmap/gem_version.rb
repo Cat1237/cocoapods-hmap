a73a3ad4f575a2fff7e5894240b1cb9d45c09102
Cat1237 <cat1237@logic.com>
2022-06-07 13:37:54 +0800

    remove USER_HEADER_SEARCH_PATHS

--- lib/hmap/gem_version.rb
+++ lib/hmap/gem_version.rb
@@ -1,5 +1,5 @@
 module HMap
   # The version of the hmapfile command line tool.
   #
-  VERSION = '0.2.6.5'.freeze unless defined? HMap::VERSION
+  VERSION = '0.2.6.6'.freeze unless defined? HMap::VERSION
 end
